567d14bcec7014850c8c0e536ffd214f5a06d6d1
Анатолий Башкатов <78781882+Bashkatov-spb@users.noreply.github.com>
2025-10-15 20:26:55 +0300

    🏷️ Export TagManager component
    - Add TagManager to component exports
    - Update index.ts for tag functionality

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { AddTodo } from './AddTodo';
 export { TodoFilters } from './TodoFilters';
 export { TodoList } from './TodoList';
 export { ErrorAlert } from './ErrorAlert';
+export { TagManager } from './TagManager';
